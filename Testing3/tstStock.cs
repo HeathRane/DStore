9094a9816aefec45b15ac4cff386a39480d020b8
Wiam Sadadi <wiamsadadi@outlook.com>
2021-02-16 08:35:07 +0000

    Added testing and class for Stock Management

---
+++ Testing3/tstStock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // instantiate class to create
+            clsStock AProduct = new clsStock();
+            // test to see if it exists
+            Assert.IsNotNull(AProduct);
+        }
+    }
+}
