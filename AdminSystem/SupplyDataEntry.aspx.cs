572b92070016cf2d57037dc03d878ac25a8a4315
Krzysztof Tomczak <P2560770@my365.dmu.ac.uk>
2021-02-26 21:04:26 +0000

    finished week 18 basically

--- AdminSystem/SupplyDataEntry.aspx.cs
+++ AdminSystem/SupplyDataEntry.aspx.cs
@@ -4,11 +4,44 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOk_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsSupply
+        clsSupply TheSupplier = new clsSupply();
+        // capture the id 
+        TheSupplier.Name = txtName.Text;
+        //Store the addres in the session object
+        Session["TheSupplier"] = TheSupplier;
+  
+        //navigate to the viewer page
+        Response.Redirect("SupplyViewer.aspx");
+    }
+
+
+
+
+
+    protected void btnOk_Click1(object sender, EventArgs e)
+    {
+        
+            //create a new instance of clsSupply
+            clsSupply TheSupplier = new clsSupply();
+            // capture the id 
+            TheSupplier.Name = txtName.Text;
+            //Store the addres in the session object
+            Session["TheSupplier"] = TheSupplier;
+
+            //navigate to the viewer page
+            Response.Redirect("SupplyViewer.aspx");
+        
     }
 }
--- Testing6/tstSupply.cs
+++ Testing6/tstSupply.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using ClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Testing6
@@ -7,10 +7,90 @@ namespace Testing6
     [TestClass]
     public class tstSupply
     {
+       
+        
+
         [TestMethod]
         public void InstanceOK()
-        { 
-          
+        {
+            clsSupply TheSupplier = new clsSupply();
+            Assert.IsNotNull(TheSupplier);
+
         }
+        [TestMethod]
+        public void InStockPropertyOK()
+        {
+            // create an instance of the class we want ot create
+            clsSupply TheSupplier = new clsSupply();
+
+            //create some test data
+            Boolean TestData = true;
+            //assign the data to the property
+            TheSupplier.InStock = TestData;
+            //tesr to see if the two values are the samme
+            Assert.AreEqual(TheSupplier.InStock, TestData);
+
+
+        }
+        [TestMethod]
+        public void DeliveryDateAddedPropertyOK()
+        {
+            clsSupply TheSupplier = new clsSupply();
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property 
+            TheSupplier.DeliveryDate = TestData;
+            Assert.AreEqual(TheSupplier.DeliveryDate, TestData);
+        }
+        [TestMethod]
+        public void ProductNamePropertyOK()
+        {
+            clsSupply TheSupplier = new clsSupply();
+            //create some test data to assign to the property
+            string TestData = "Name";
+            //assign the data to the property
+            TheSupplier.Name = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(TheSupplier.Name, TestData);
+        }
+
+        [TestMethod]
+        public void ProductIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsSupply TheSupplier = new clsSupply();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            TheSupplier.ProductId = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(TheSupplier.ProductId, TestData);
+        }
+        [TestMethod]
+        public void QuantityNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsSupply TheSupplier = new clsSupply();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            TheSupplier.QuantityNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(TheSupplier.QuantityNo, TestData);
+        }
+
+        [TestMethod]
+        public void PricePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsSupply TheSupplier = new clsSupply();
+            //create some test data to assign to the property
+            double TestData = 1.5;
+            //assign the data to the property
+            TheSupplier.Price = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(TheSupplier.Price, TestData);
+        }
+
+
     }
 }
